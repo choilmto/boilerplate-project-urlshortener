b4ee20011b4f341ef002419444023fe25074dffb
choilm <choilmto@gmail.com>
2020-09-22 18:19:48 -0400

    Add timer to keep process alive long enough for db to connect.

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const { createApp, startServer } = require("./app");
 startServer(createApp).catch((err) =>
   logger.fatal(err, "Server crash as db failed to connect.")
 );
+
+setInterval(function () { }, 60000);
